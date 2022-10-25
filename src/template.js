7ff98d1e6f53b3ec327aff4c994ad5f468b94094
Angellyn218 <angellyncervantes@yahoo.com>
2022-10-25 15:22:32 -0700

    Added renderEmployees function

--- src/template.js
+++ src/template.js
@@ -1,6 +1,39 @@
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+function renderEmployees(team) {
+    const employees = ``;
+    for (let i = 0; i < team.length; i++) {
+        const third = ``;
+        if (team[i].getRole() === "Manager") {
+            const third = `Office Number: ${team[i].officeNumber}`;
+        } else if (team[i].getRole() === "Engineer") {
+            const third = `Github: <a href="${team[i].getGithub()}" class="card-link">${team[i].getGithub()}</a>`;
+        } else {
+            const third = `School: ${team[i].school}`
+        }
+        const str = `<div class="card">
+        <div class="card-header">
+            <h5 class="card-title">${team[i].getName()}</h5>
+            <h6 class="card-subtitle">${team[i].getRole()}</h6>
+        </div>
+        <div class="card-body">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${team[i].getId()}</li>
+                <li class="list-group-item">Email: <a href="${team[i].getEmail()}" class="card-link">${team[i].getEmail()}</a></li>
+                <li class="list-group-item">${third}</li>
+            </ul>
+        </div>
+    </div>`
+        employees += str;
+        employees += `\n`
+    }
+    return employees;
+}
 
 function renderTeamSection(team) {
-    const employeesEl = renderEmployees();
+    const employeesEl = renderEmployees(team);
     return `<div class="row align-items-center justify-content-evenly team">
     ${employeesEl}
     </div>`
