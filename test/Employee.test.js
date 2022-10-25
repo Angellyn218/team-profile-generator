81e326bd32959aaf22fef66d7e51f56bc29651cc
Angellyn218 <angellyncervantes@yahoo.com>
2022-10-24 22:33:41 -0700

    Manager tests

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -14,101 +14,96 @@ const egId = 1;
 const egEmail = "BobTheBuilder@example.com";
 
 describe("Employee class", () => {
-    describe("Initialization", () => {
-        // Positive test
-        it("Properties of the employee are set on initialization", () => {
-            const obj = new Employee(egName, egId, egEmail);
-    
-            expect(obj.name).toEqual(egName);
-            expect(obj.id).toEqual(egId);
-            expect(obj.email).toEqual(egEmail);
-        })
+    // Initialization
+    // Positive test
+    it("Properties of the employee are set on initialization", () => {
+        const obj = new Employee(egName, egId, egEmail);
 
-        it("Throw error: No parameters", () => {
-            const cb = () => new Employee();
-            const err = new Error(
-                "Expected 3 parameters, recieved 0"
-            );
-
-            // Assert
-            expect(cb).toThrowError(err);
-        })
-
-        it("Throw error: No name", () => {
-            const cb = () => new Employee(egName);
-            const err = new Error(
-                "Expected 3 parameters, recieved 1"
-            );
-
-            // Assert
-            expect(cb).toThrowError(err);
-        })
-
-        it("Throw error: No name", () => {
-            const cb = () => new Employee(egName, egId);
-            const err = new Error(
-                "Expected 3 parameters, recieved 2"
-            );
-
-            // Assert
-            expect(cb).toThrowError(err);
-        })
-
-        it("Throw error: Name not string", () => {
-            const cb = () => new Employee(egId, egId, egEmail);
-            const err = new Error(
-                "Expected a string as the name parameter, recived a non-string"
-            );
-
-            // Assert
-            expect(cb).toThrowError(err);
-        })
-
-        it("Throw error: Name not number", () => {
-            const cb = () => new Employee(egName, egName, egEmail);
-            const err = new Error(
-                "Expected a number as the id parameter, recived non-number"
-            );
-
-            // Assert
-            expect(cb).toThrowError(err);
-        })
-
-        it("Throw error: Name not string", () => {
-            const cb = () => new Employee(egName, egId, egId);
-            const err = new Error(
-                "Expected a string as the email parameter, recived non-string"
-            );
-
-            // Assert
-            expect(cb).toThrowError(err);
-        })
+        expect(obj.name).toEqual(egName);
+        expect(obj.id).toEqual(egId);
+        expect(obj.email).toEqual(egEmail);
     })
 
-    describe("Methods", () => {
-        it("getName method returns the name of employee", () => {
-            const obj = new Employee(egName, egId, egEmail);
+    // Throw errors
+    it("Throw error: No parameters", () => {
+        const cb = () => new Employee();
 
-            expect(obj.getName()).toBe(egName);
-        })
+        // Assert
+        expect(cb).toThrow();
+    })
 
-        it("getName method returns the name of employee", () => {
-            const obj = new Employee(egName, egId, egEmail);
+    it("Throw error: id not a number", () => {
+        const cb = () => new Employee(egName);
+        const err = new Error(
+            "Expected a number as the id parameter"
+        );
 
-            expect(obj.getId()).toBe(egId);
-        })
+        // Assert
+        expect(cb).toThrowError(err);
+    })
 
-        it("getName method returns the name of employee", () => {
-            const obj = new Employee(egName, egId, egEmail);
+    it("Throw error: email not a string", () => {
+        const cb = () => new Employee(egName, egId);
+        const err = new Error(
+            "Expected a string as the email parameter"
+        );
 
-            expect(obj.getEmail()).toBe(egEmail);
-        })
+        // Assert
+        expect(cb).toThrowError(err);
+    })
 
-        it("getName method returns the name of employee", () => {
-            const obj = new Employee(egName, egId, egEmail);
+    it("Throw error: Name not number", () => {
+        const cb = () => new Employee(egId, egId, egEmail);
+        const err = new Error(
+            "Expected a string as the name parameter"
+        );
 
-            expect(obj.getRole()).toBe("Employee");
-        })
+        // Assert
+        expect(cb).toThrowError(err);
+    })
 
+    it("Throw error: Id not string", () => {
+        const cb = () => new Employee(egName, egName, egEmail);
+        const err = new Error(
+            "Expected a number as the id parameter"
+        );
+
+        // Assert
+        expect(cb).toThrowError(err);
+    })
+
+    it("Throw error: Email not string", () => {
+        const cb = () => new Employee(egName, egId, egId);
+        const err = new Error(
+            "Expected a string as the email parameter"
+        );
+
+        // Assert
+        expect(cb).toThrowError(err);
+    })
+
+    // Methods
+    it("getName method returns the name of employee", () => {
+        const obj = new Employee(egName, egId, egEmail);
+
+        expect(obj.getName()).toBe(egName);
+    })
+
+    it("getId method returns the name of employee", () => {
+        const obj = new Employee(egName, egId, egEmail);
+
+        expect(obj.getId()).toBe(egId);
+    })
+
+    it("getEmail method returns the name of employee", () => {
+        const obj = new Employee(egName, egId, egEmail);
+
+        expect(obj.getEmail()).toBe(egEmail);
+    })
+
+    it("get method returns the name of employee", () => {
+        const obj = new Employee(egName, egId, egEmail);
+
+        expect(obj.getRole()).toBe("Employee");
     })
 })
---
+++ test/Manager.test.js
@@ -0,0 +1,106 @@
+// officeNumber
+// getRole()—overridden to return 'Manager'
+
+const Manager = require('../lib/Manager');
+const egName = "Bob";
+const egId = 1;
+const egEmail = "BobTheBuilder@example.com";
+const egOfficeNumber = 1;
+
+describe("Manager Class", () => {
+    // Initialization
+    // Positive Test
+    it("Properties of the employee are set on initialization", () => {
+        const obj = new Manager(egName, egId, egEmail, egOfficeNumber);
+
+        expect(obj.name).toEqual(egName);
+        expect(obj.id).toEqual(egId);
+        expect(obj.email).toEqual(egEmail);
+        expect(obj.officeNumber).toEqual(egOfficeNumber);
+    })
+
+    // Throw Errors
+    it("Throw error: No parameters", () => {
+        const cb = () => new Manager();
+
+        // Assert
+        expect(cb).toThrow();
+    })
+
+    it("Throw error: id not a number", () => {
+        const cb = () => new Manager(egName);
+        const err = new Error(
+            "Expected a number as the id parameter"
+        );
+
+        // Assert
+        expect(cb).toThrowError(err);
+    })
+
+    it("Throw error: email not a string", () => {
+        const cb = () => new Manager(egName, egId);
+        const err = new Error(
+            "Expected a string as the email parameter"
+        );
+
+        // Assert
+        expect(cb).toThrowError(err);
+    })
+
+    it("Throw error: officeNumber not a number", () => {
+        const cb = () => new Manager(egName, egId, egEmail);
+        const err = new Error(
+            "Expected a number as the office number parameter"
+        );
+
+        // Assert
+        expect(cb).toThrowError(err);
+    })
+
+    it("Throw error: Name not number", () => {
+        const cb = () => new Manager(egId, egId, egEmail, egOfficeNumber);
+        const err = new Error(
+            "Expected a string as the name parameter"
+        );
+
+        // Assert
+        expect(cb).toThrowError(err);
+    })
+
+    it("Throw error: Id not string", () => {
+        const cb = () => new Manager(egName, egName, egEmail, egOfficeNumber);
+        const err = new Error(
+            "Expected a number as the id parameter"
+        );
+
+        // Assert
+        expect(cb).toThrowError(err);
+    })
+
+    it("Throw error: Email not string", () => {
+        const cb = () => new Manager(egName, egId, egId, egOfficeNumber);
+        const err = new Error(
+            "Expected a string as the email parameter"
+        );
+
+        // Assert
+        expect(cb).toThrowError(err);
+    })
+
+    it("Throw error: Office Number not number", () => {
+        const cb = () => new Manager(egName, egId, egEmail, egEmail);
+        const err = new Error(
+            "Expected a number as the office number parameter"
+        );
+
+        // Assert
+        expect(cb).toThrowError(err);
+    })
+
+    // Methods
+    it("getRole method returns 'Manager' as role", () => {
+        const obj = new Manager(egName, egId, egEmail, egOfficeNumber);
+
+            expect(obj.getRole()).toBe("Manager");
+    })
+}) 
